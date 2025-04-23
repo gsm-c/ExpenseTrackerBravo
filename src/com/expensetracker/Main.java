e4c4ef08b197d34589288c8f4b9229993f29ff9c
gurpaul <gurpaulmann@gmail.com>
2025-04-22 19:51:58 -0700

    Version 1.01

--- src/com/expensetracker/Main.java
+++ src/com/expensetracker/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
         // Initialize database
         DatabaseManager.initializeDatabase();
-
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
